ff3affa6d61d642c759746d4ede9596359631adb
Diego Nieto Cid <dnietoc@gmail.com>
2016-10-15 19:37:35 -0300

    Rename require function to avoid Meteor static analysis

--- gulpfile.js
+++ gulpfile.js
@@ -3,6 +3,7 @@ var rename = require('gulp-rename');
 var wrap = require('gulp-wrap');
 var clean  = require('gulp-clean');
 var run = require('gulp-run');
+var replace = require('gulp-replace');
 
 gulp.task('clean-dist', function () {
 	return gulp.src(['./dist', './tmp'], {read: false})
@@ -32,6 +33,12 @@ gulp.task('bundle-forge', ['patch-forge'], function () {
 
 gulp.task('install-forge', ['bundle-forge'], function () {
 	return gulp.src('./tmp/forge/js/forge.bundle.js')
+			.pipe(replace(/return require/g, "return require_internal"))
+			.pipe(replace(/(.*, )require(,.*)/g, "$1require_internal$2"))
+			.pipe(replace(/(.* )require( =.*)/g, "$1require_internal$2"))
+			.pipe(replace(/(.*factory\()require(,.*)/g,"$1require_internal$2"))
+			.pipe(replace(/(.*function\()require(,.*)/g,"$1require_internal$2"))
+			.pipe(replace(/(.*)require(\('crypto.*)/g, "$1require_internal$2"))
 			.pipe(rename('forge.js'))
 			.pipe(gulp.dest('./dist'));
 });
